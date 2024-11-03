88e94bbcce50e62deed53223fdf505a72a921a50
tatcher <tatcher@ecwid.com>
2024-11-04 01:02:53 +0400

    added drag-and-drop

--- frontend/src/common/helpers.ts
+++ frontend/src/common/helpers.ts
@@ -1,4 +1,5 @@
 export const getImage = (image) => {
   // https://vitejs.dev/guide/assets.html#new-url-url-import-meta-url
+  //@ts-ignore
   return new URL(`../assets/img/${image}`, import.meta.url).href;
 };
